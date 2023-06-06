21718b11dd3f7a0c71d62b54d8e38497b7acc98b
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-06-06 12:41:26 +0300

    .

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -4,7 +4,7 @@ import s from "./Preloader.module.css";
 const Preloader = (props) => {
     return (
         <div className={s.preloader}>
-            <img src="./loader.svg"/>
+            <img src="./loader.svg" alt="loader"/>
         </div>
     )
 }
